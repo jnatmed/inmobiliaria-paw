68bb6857f400ed1fba96308729fd1fcbec249fdb
jnatmed <49349742+jnatmed@users.noreply.github.com>
2024-06-02 21:26:01 -0300

    siguiendo con el maquetado
    imagenes y demas elementos del dom

--- src/App/views/parts/header.view.php
+++ src/App/views/parts/header.view.php
@@ -1,6 +1,6 @@
 <header> <!--block-->
 
-    <h1>PawPerties</h1>
+    <h1 class="icono-header-pawperties">PawPerties</h1>
     <nav class="nav-index">
         <ul class="lista-nav-main">
             <li>
