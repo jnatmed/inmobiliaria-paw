01689bb62bcf094f6c802eebfef8434164676578
MatíasCM1 <mcosentino020@mail.com>
2024-06-13 15:56:35 -0300

    cambios en el base.css

--- src/App/views/buscar-propiedad.view.php
+++ src/App/views/buscar-propiedad.view.php
@@ -14,7 +14,19 @@
 
     <main>
 
-    <?php require __DIR__.'/parts/mapa.view.php' ?>
+    <section class="seccion-busqueda">
+            <form action="" class="form-busqueda-propiedad" method="GET">
+                <label for="input-zona" class="label-form" id="input-zona">Zona</label>
+                <input type="text" class="input-form-busqueda" placeholder="Elegi tu Zona...">
+                <label for="input-tipo" class="label-form">Tipo</label>
+                <input type="text" class="input-form-busqueda" id="input-tipo" placeholder="Tipo">
+                <label for="input-categoria" class="label-form">Categoria</label>
+                <input type="text" class="input-form-busqueda" id="input-categoria" placeholder="Categoria">
+                <input type="submit" value="Buscar" class="btn-form-busqueda">
+            </form>
+
+        <?php require __DIR__.'/parts/mapa.view.php' ?>
+    </section>
 
     </main>
     
