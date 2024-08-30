568d18c073c2ba6805a1c6596e2dcf0f6832a55a
lucasrueda01 <luquirueda01@gmail.com>
2024-08-30 16:44:38 -0300

    navbar responsive

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -2,10 +2,6 @@ class appPAW {
   constructor() {
     document.addEventListener('DOMContentLoaded', () => {
 
-      document.querySelector('.menu-toggle').addEventListener('click', () => {
-        document.querySelector('.nav-index').classList.toggle('active');
-      });
-
     });
   }
 }
